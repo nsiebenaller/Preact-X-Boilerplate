f959ae74e1c3d2ea85a3270738f6c4ad2aad39bc
Nicholas <33887782+nsiebenaller@users.noreply.github.com>
2019-10-18 13:12:43 -0500

    Init Commit

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import { h, Component } from 'preact';
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render(props, state) {
+    return(
+      <div>Welcome to My App</div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { h, Component, render } from 'preact';
+import App from './App/App.js'
+
+render(
+  <App />,
+  document.getElementsByTagName("BODY")[0]
+);
